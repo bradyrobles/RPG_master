c9759df8a8dcdc0006e0028c7c815640c7a2e682
bradyrobles <you@example.com>
2020-10-28 13:59:19 -0400

    load tilemap keyed 'map'

--- assets/js/scenes/BootScene.js
+++ assets/js/scenes/BootScene.js
@@ -10,11 +10,15 @@ class BootScene extends Phaser.Scene {
     this.loadSpriteSheets();
     // load audio
     this.loadAudio();
+    // load tilemap
+    this.loadTileMap();
   }
 
   loadImages() {
     this.load.image('button1', 'assets/images/ui/blue_button01.png');
     this.load.image('button2', 'assets/images/ui/blue_button02.png');
+    // load the map tileset image
+    this.load.image('background', 'assets/level/background-extruded.png');
   }
 
   loadSpriteSheets() {
@@ -25,8 +29,13 @@ class BootScene extends Phaser.Scene {
   loadAudio() {
     this.load.audio('goldSound', ['assets/audio/Pickup.wav']);
   }
+  
+  loadTileMap(){
+    // map made with Tiled in .json
+    this.load.tilemapTiledJSON('map', 'assets/level/large_level.json');
+  }
 
   create() {
-    this.scene.start('Title');
+    this.scene.start('Game');
   }
 }
