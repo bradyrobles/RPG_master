01b5a55f0b4f503d7121508e55e4108c10bd8e67
bradyrobles <you@example.com>
2020-11-01 13:20:16 -0500

    refactored map into new class

--- assets/js/scenes/GameScene.js
+++ assets/js/scenes/GameScene.js
@@ -12,22 +12,22 @@ class GameScene extends Phaser.Scene {
     this.createMap();
     this.createAudio();
     this.createChests();
-    this.createPlayer();
-    this.addCollisions();
     this.createInput();
+
+    this.createGameManager();
     
   }
 
   update() {
-    this.player.update(this.cursors);
+    if (this.player) this.player.update(this.cursors);
   }
 
   createAudio() {
     this.goldPickupAudio = this.sound.add('goldSound', { loop: false, volume: 0.2 });
   }
 
-  createPlayer() {
-    this.player = new Player(this, 32, 32, 'characters', 7);
+  createPlayer(location) {
+    this.player = new Player(this, location[0] *2, location[1] *2, 'characters', 7);
   }
 
   createChests() {
@@ -64,8 +64,8 @@ class GameScene extends Phaser.Scene {
   }
 
   addCollisions() {
-    // check for collisions between player and wall objects
-    this.physics.add.collider(this.player, this.wall);
+    // check for collisions between player and Tiled blocked layer
+    this.physics.add.collider(this.player, this.map.blockedLayer);
     // check for overlaps between player and chest game objects
     this.physics.add.overlap(this.player, this.chests, this.collectChest, null, this);
   }
@@ -84,22 +84,16 @@ class GameScene extends Phaser.Scene {
   }
 
   createMap(){
-    // create the tile map, 'map' refers to variable assigned in BootScene.loadTileMap()
-    this.map = this.make.tilemap({ key: 'map'});
-    // add tileset image to map
-    this.tiles = this.map.addTilesetImage('background', 'background', 32, 32, 1, 2);
-    // create background layer
-    this.backgroundLayer = this.map.createStaticLayer('background', this.tiles, 0, 0);
-    this.backgroundLayer.setScale(2);
-    // create blocked layer
-    this.blockedLayer = this.map.createStaticLayer('blocked', this.tiles, 0, 0);
-    this.blockedLayer.setScale(2);
-    
-    // update world bounds; *2 because we already scaled up map
-    this.physics.world.bounds.width = this.map.widthInPixels *2;
-    this.physics.world.bounds.height = this.map.heightInPixels *2;
+    // create map
+    this.map = new Map(this, 'map', 'background', 'background', 'blocked');
+  }
 
-    // limit camera to (x,y) of our map
-    this.cameras.main.setBounds(0, 0, this.map.widthInPixels *2, this.map.heightInPixels *2);
+  createGameManager(){
+    this.events.on('spawnPlayer', (location) => {
+      this.createPlayer(location);
+      this.addCollisions();
+    });
+    this.gameManager = new GameManager(this, this.map.map.objects) //.map field of this.map is the .json tilemap
+    this.gameManager.setup();
   }
 }
