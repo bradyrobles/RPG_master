71d730dae1e6ea1d18ea2792b4d59a4c92a695cd
bradyrobles <you@example.com>
2020-11-02 12:23:01 -0500

    refactored createChests

--- assets/js/scenes/GameScene.js
+++ assets/js/scenes/GameScene.js
@@ -11,7 +11,7 @@ class GameScene extends Phaser.Scene {
   create() {
     this.createMap();
     this.createAudio();
-    this.createChests();
+    this.createGroups();
     this.createInput();
 
     this.createGameManager();
@@ -30,31 +30,60 @@ class GameScene extends Phaser.Scene {
     this.player = new Player(this, location[0] *2, location[1] *2, 'characters', 7);
   }
 
-  createChests() {
+  createGroups() {
     // create a chest group
     this.chests = this.physics.add.group();
-    // create chest positions array
-    this.chestPositions = [[100, 100], [200, 200], [300, 300], [400, 400], [500, 500]];
-    // specify the max number of chest we can have
-    this.maxNumberOfChests = 3;
-    // spawn a chest
-    for (let i = 0; i < this.maxNumberOfChests; i += 1) {
-      this.spawnChest();
-    }
+
+    // create a monster group
+    this.monsters = this.physics.add.group();
+    
   }
 
-  spawnChest() {
-    const location = this.chestPositions[Math.floor(Math.random() * this.chestPositions.length)];
-
+  spawnChest(chestObject) {
     let chest = this.chests.getFirstDead();
 
     if (!chest) {
-      const chest = new Chest(this, location[0], location[1], 'items', 0);
+      chest = new Chest(this, 
+        chestObject.x * scaleFactor, 
+        chestObject.y * scaleFactor, 
+        'items', 
+        0, 
+        chestObject.gold, 
+        chestObject.id);
+        
       // add chest to chests group
       this.chests.add(chest);
     } else {
-      chest.setPosition(location[0], location[1]);
+      chest.coins = chestObject.gold;
+      chest.id = chestObject.id;
+      chest.setPosition(chestObject.x * scaleFactor, chestObject.y * scaleFactor);
       chest.makeActive();
+    }
+  }
+
+  spawnMonster(monsterObject){
+    let monster = this.monsters.getFirstDead();
+
+    if (!monster) {
+      monster = new Monster(this, 
+        monsterObject.x * scaleFactor, 
+        monsterObject.y * scaleFactor, 
+        'monsters', 
+        monsterObject.frame, 
+        monsterObject.id,
+        monsterObject.health,
+        monsterObject.maxHealth,
+        );
+
+      // add monster to monsters group
+      this.monsters.add(monster);
+    } else {
+      monster.id = monsterObject.id;
+      monster.health = monsterObject.health;
+      monster.maxHealth = monsterObject.maxHealth;
+      monster.setTexture('monsters', monsterObject.frame);
+      monster.setPosition(monsterObject.x * scaleFactor, monsterObject.y * scaleFactor);
+      monster.makeActive();
     }
   }
 
@@ -80,7 +109,7 @@ class GameScene extends Phaser.Scene {
     // make chest game object inactive
     chest.makeInactive();
     // spawn a new chest
-    this.time.delayedCall(1000, this.spawnChest, [], this);
+    this.events.emit('pickupChest', chest.id);
   }
 
   createMap(){
@@ -89,10 +118,24 @@ class GameScene extends Phaser.Scene {
   }
 
   createGameManager(){
+    // Listener Event for spawning player
     this.events.on('spawnPlayer', (location) => {
       this.createPlayer(location);
       this.addCollisions();
     });
+
+    // Listener Event for spawning chests
+    this.events.on('chestSpawned', (chest) => {
+      this.spawnChest(chest);
+      //console.log(chest);
+    });
+
+    // Listener Event for spawning monsters
+    this.events.on('monsterSpawned', (monster) => {
+      this.spawnMonster(monster);
+    });
+
+    // create the game manager
     this.gameManager = new GameManager(this, this.map.map.objects) //.map field of this.map is the .json tilemap
     this.gameManager.setup();
   }
